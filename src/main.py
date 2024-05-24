9aac4919a2c1f18288e42ef16552d93ca3467c55
KirillMaltsev3341 <kirill57m@gmail.com>
2024-05-24 17:49:33 +0300

    backup

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
-import argparse
 from handler import VideoHandler
 
 
 def main():
     
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("-i", "--input",   help="input file name",   default="input.mp4")
-    parser.add_argument("-o", "--output",  help="output file name",  default="output.mp4")
-    parser.add_argument("-r", "--report",  help="report file name",  default="report.csv")    
-    
-    args = parser.parse_args()
+    input_file_name = input("input file name: ")
+    output_file_name = "output.mp4"
+    report = "report.csv"
     
     video_handler = VideoHandler()
-    video_handler.process_video("../input/"+args.input, "../output/"+args.output, "../output/"+args.report)
-    
+    video_handler.process_video("../input/"+input_file_name, "../output/"+output_file_name, "../output/"+report)
 
 
 if __name__ == '__main__':
