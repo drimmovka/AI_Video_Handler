385b2bace5dc4440c8891bb5f8db89e4296953b4
KirillMaltsev3341 <144445070+KirillMaltsev3341@users.noreply.github.com>
2024-05-24 14:45:42 +0300

    Update install_required_software.sh

--- install_required_software.sh
+++ install_required_software.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get upgrade
 apt-get install python3 -y
 apt-get install python3-pip -y
-apt-get install libgl1
+apt-get install libgl1 -y
 
 pip3 install ultralytics
 pip3 install ultralytics --upgrade
